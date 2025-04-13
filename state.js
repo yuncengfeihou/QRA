cc5d9f14e2a3a9c283726daf4253f6a6ce569e54
yuncengfeihou <117951839+yuncengfeihou@users.noreply.github.com>
2025-04-14 00:02:00 +0800

    Update state.js

--- state.js
+++ state.js
@@ -4,27 +4,11 @@
 export const sharedState = {
     menuVisible: false,
     domElements: {
-        // Existing elements
         rocketButton: null,
         menu: null,
         chatItemsContainer: null,
         globalItemsContainer: null,
         settingsDropdown: null,
-        // New elements for icon settings
-        customizeIconButton: null,
-        iconSettingsModal: null,
-        iconSettingsBackdrop: null,
-        iconTypeSelect: null,
-        iconSvgInputContainer: null,
-        iconSvgInput: null,
-        iconUrlInputContainer: null,
-        iconUrlInput: null,
-        iconSettingsSaveButton: null,
-        iconSettingsCloseButton: null,
-        iconPreview: null, // Reference to preview area
-        iconColorPicker: null,
-        iconHoverColorPicker: null,
-        iconActiveColorPicker: null,
     }
 };
 
